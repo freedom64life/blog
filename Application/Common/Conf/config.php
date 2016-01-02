dc7f9baef1e9c5ed6457acc5e3b0ce2b252b7913
liwq <nick@pzfresh.com>
2016-01-02 21:57:40 +0800

    单入口，多域名配置模式

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	
+	'APP_SUB_DOMAIN_DEPLOY'   =>    1, // 开启子域名或者IP配置
+	// 多域名
+	'APP_SUB_DOMAIN_RULES'    =>    array( 
+			'www.ace.com'			=>	'Www',
+			'www.ace-admin.com' 	=>  'Admin',
+// 			'www.ace-wx.com'		=>  'Wx',
+// 			'www.ace-api.com'		=>  'Api',
+	)
+);
