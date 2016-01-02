7ab9eac24fbc4d8f53314c511885400e0eb60339
liwq <nick@pzfresh.com>
2016-01-03 00:29:53 +0800

    ace 框架文件提交

---
+++ Application/Admin/Controller/AdminController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 后台公共入口
+ * @author Nick
+ *
+ */
+namespace Admin\Controller;
+use Think\Controller;
+class AdminController extends Controller {
+    public function __construct(){
+    	parent::__construct();
+    	
+    }
+
+}
